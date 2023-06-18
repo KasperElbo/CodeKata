939eaaf2c0f6d060e337e3876449893a5e04375d
Kasper Elbo <ekel@dr.dk>
2023-06-19 01:39:25 +0200

    Bowling Game Init

---
+++ CodeKata/CodeKata.Solutions.Test/1_Bowling/BowlingTests.cs
@@ -0,0 +1,29 @@
+using CodeKata.Solutions._1_Bowling;
+using Xunit.Sdk;
+
+namespace CodeKata.Solutions.Test._1_Bowling
+{
+    public class BowlingTests
+    {
+        
+        public BowlingTests()
+        {
+            
+        }
+
+        public class SomeBowlingGameMethod : BowlingTests
+        {
+            [Fact]
+            public void BowlingGame_FirstTest_IsATest()
+            {
+                
+            }
+        }
+
+        private Bowling CreateTarget()
+        {
+            
+            return new Bowling();
+        }
+    }
+}
---
+++ CodeKata/CodeKata.Solutions/1_Bowling/Bowling.cs
@@ -0,0 +1,15 @@
+﻿using CodeKata.Common;
+
+namespace CodeKata.Solutions._1_Bowling
+{
+    public class Bowling : ICodeKata
+    {
+        public int Index => 1;
+        public string Name => "Bowling Game";
+
+        public void Execute()
+        {
+            Console.WriteLine($"{Name} Called");
+        }
+    }
+}
